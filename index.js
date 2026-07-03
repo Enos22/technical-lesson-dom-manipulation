8f1f9659d2e04710bfdbee9901391f648856e040
Emos Arenga <enos.arenga@gmail.com>
2026-07-03 20:56:51 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-// Write your code here!
+const userInfo = {
+	username: "flatUser123",
+	description: "Hi, welcome to my webpage! Just a new user of this website!",
+	theme: "red"
+}
+
+function displayUser(userInfo){
+    const header = document.querySelector("#main")
+    header.textContent = userInfo.username + "'s Personal Webpage" //changes the content of the webpage.
+    header.className = userInfo.theme //changes the name of the class applied to any piece of DOM.
+
+    const description = document.createElement("p")
+    const body = document.querySelector("body")
+
+    description.textContent = userInfo.description
+    description.className = userInfo.theme
+
+    body.appendChild(description) //adds the description to the body of the webpage.
+    }
+displayUser(userInfo)
 
